77e9dce43cdf6f68423d9ecf1b4643d9a7cff836
Erdem Ozgen <m.erdemozgen@gmail.com>
2020-07-10 01:11:52 +0300

    Added First Chapter2 first code

---
+++ sourceCodeOfNutshell/Chapter2/FirstCSharp.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+class Test
+{
+    static void Main()
+    {
+        int x =12;
+        Console.WriteLine(x);
+        Console.WriteLine(
+            "Hello World!!!"
+        );
+    }
+}
